e8bcedb0798eb44d42ef39b25753c8f1e7a31777
Shravan Pawar <shravan@example.com>
2026-05-30 20:54:56 +0530

    fix: CORS allow all origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -55,7 +55,7 @@ def health_check():
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
